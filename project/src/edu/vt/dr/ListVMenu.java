859e8e58fafbefd9143b0944cc8a5f39991f023f
kimdo0305 <kimdo0305@gmail.com>
2012-04-11 23:49:26 +0000

    MapList page update

--- project/src/edu/vt/dr/ListVMenu.java
+++ project/src/edu/vt/dr/ListVMenu.java
@@ -1,54 +1,61 @@
 package edu.vt.dr;
 
-import android.app.ListActivity;
+import android.app.Activity;
 import android.content.Intent;
 import android.content.pm.ActivityInfo;
 import android.os.Bundle;
 import android.view.View;
-import android.view.Window;
-import android.widget.AdapterView;
-import android.widget.AdapterView.OnItemClickListener;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
-import android.widget.TextView;
 
-public class ListVMenu extends ListActivity {
+import android.widget.ImageButton;
+
+
+public class ListVMenu extends Activity {
 	
-	static final String[] mapOptions =new String[]{
-		"PAM 3010", "Straight Map", "Circle Map", "LShapeMap"
-	};
+	private static final String PAM3010 = "edu.vt.dr.PAM3010MAP";
+	private static final String STRAIGHT = "edu.vt.dr.STRAIGHTMAP";
+	private static final String CIRCLE = "edu.vt.dr.CIRCLEMAP";
 
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
+	
+	/** Called when the activity is first created. */
+    public void onCreate(Bundle savedInstanceState) {
+    	//calls super, sets GUI
+        super.onCreate(savedInstanceState);
 		setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
-		requestWindowFeature(Window.FEATURE_NO_TITLE);
-		
-		setListAdapter( new ArrayAdapter<String>(this, R.layout.single_item, mapOptions));
-		ListView lv = getListView();
-		lv.setTextFilterEnabled(true);
-		lv.setOnItemClickListener(new OnItemClickListener(){
+        setContentView(R.layout.maplist);
+        
+        //associates buttons with IDs
+        
+        ImageButton pam3010Button = (ImageButton) findViewById(R.id.buttonPam3010);
+        ImageButton straightButton = (ImageButton) findViewById(R.id.buttonStraight);
+        ImageButton circleButton = (ImageButton) findViewById(R.id.buttonCircle);
+                
+        //associates listener for Map Selection button
+        pam3010Button.setOnClickListener(new View.OnClickListener(){
 
-			public void onItemClick(AdapterView<?> arg0, View arg1, int arg2,
-					long arg3) {
-				String s = (String) ((TextView) arg1).getText();
-				if(s == "PAM 3010" )
-				{
-					s = "PAM3010MAP";
-				}
-				else{
-				s=s.replace(" ","");
-				s=s.toUpperCase();
-				}
-			    String ns = "edu.vt.dr." + s;
-				Intent gotoMenu = new Intent(ns);
-				startActivity(gotoMenu);			
+			public void onClick(View v) {
+		      Intent i = new Intent(PAM3010);
+              startActivity(i);
 			}
+        });
+        
+        //associates listener for About Us Button
+        straightButton.setOnClickListener(new View.OnClickListener() {
 			
-			
+			public void onClick(View v) {
+				Intent i = new Intent(STRAIGHT);
+				startActivity(i);
+				
+			}
 		});
-	}
-	
-	
-
+        
+        
+        circleButton.setOnClickListener(new View.OnClickListener() {
+			
+			public void onClick(View v) {
+				Intent i = new Intent(CIRCLE);
+				startActivity(i);
+			}
+		});
+       
+    }
 }
