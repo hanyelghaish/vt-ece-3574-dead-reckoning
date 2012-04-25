504762eb3515f444a6194cf4987ce9476d968a44
kimdo0305@gmail.com <kimdo0305@gmail.com@fdebf5bf-d676-0c76-f7cf-3c8edb0e5aaa>
2012-04-25 22:57:04 +0000

    Integrate moving dot on straight, circle, and L-shaped map

--- trunk/project/src/edu/vt/dr/CircleMap.java
+++ trunk/project/src/edu/vt/dr/CircleMap.java
@@ -1,7 +1,20 @@
 package edu.vt.dr;
+import edu.vt.dr.map.circleGLRenderer;
+import edu.vt.dr.map.straightGLRenderer;
+import edu.vt.dr.testing.utilities.FloatPoint;
+import edu.vt.dr.testing.utilities.LocationUtil;
+import edu.vt.dr.testing.utilities.SensorUtil;
 import android.app.Activity;
+import android.content.Context;
 import android.content.pm.ActivityInfo;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.opengl.GLSurfaceView;
 import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.Window;
+import android.widget.TextView;
 
 /******************************************************************************
  * 
@@ -21,12 +34,108 @@ import android.os.Bundle;
  * 
  ******************************************************************************/
 
-public class CircleMap extends Activity{
+public class CircleMap extends Activity implements SensorEventListener{
 	
+private GLSurfaceView ciclemGLView;
+ 
+	
+
+	
+	
+	/**************************************************
+	 * On Create
+	**************************************************/
+	
+	private boolean sysOk;
+	private SensorUtil SU;
+	private TextView tv;
+	
+	@Override
 	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
 		super.onCreate(savedInstanceState);
 		setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
-		setContentView(R.layout.circle);
+		
+		//MIKE'S
+		 SU = new SensorUtil(this);
+	        LocationUtil.init();
+	        if (SU.systemMeetsRequirements()) {
+	        	
+	        	requestWindowFeature(Window.FEATURE_NO_TITLE);
+	        	setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+	        	
+	        
+	        	sysOk = true;
+	        
+	        } else {
+	        	//system does not need requirement
+	        	sysOk = false;
+	        }
+	      //--------------------
+		
+		
+		//setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+	    ciclemGLView = new circleSurfaceView(this);
+		setContentView(ciclemGLView); 
+		
+		 
 	}
 
+		
+	
+	/**************************************************
+	 * On Pause
+	**************************************************/
+	@Override
+	protected void onPause(){
+		super.onPause();
+		if (sysOk) {
+    		SU.unregisterListeners();
+    		ciclemGLView.onPause();
+    	}
+	}
+	
+	/**************************************************
+	 * On Resume
+	**************************************************/
+	@Override
+	protected void onResume(){
+		super.onResume();
+		if (sysOk) {
+    		SU.registerListeners();
+    		ciclemGLView.onResume();
+    	}
+	}
+
+	
+
+	@Override 
+    public boolean onTouchEvent(MotionEvent event) {
+    	LocationUtil.reset();
+        return true; 
+    } 
+
+    public void onSensorChanged(SensorEvent event) {
+    	
+    	FloatPoint f = LocationUtil.getCurrentLocation();
+    	
+    	SU.routeEvent(event);
+    	
+    }
+    
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+    	
+    }
+	
+}
+
+class circleSurfaceView extends GLSurfaceView{
+
+	public circleSurfaceView(Context context) {
+		super(context);
+		// TODO Auto-generated constructor stub
+		
+		//Set the Renderer for drawing on the GLSurfaceView
+		setRenderer(new circleGLRenderer(context));
+	}
 }
